6d73baca2a6c46b7101d03494653d53b9bb2f7dc
Justas T <justas.tam@gmail.com>
2025-03-02 10:53:04 +0200

    register modules to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Greeting
+from .models import Greeting, Country, League, Characteristic, FootballClub
 
 # Register your models here.
 admin.site.register(Greeting)
+admin.site.register(Country)
+admin.site.register(League)
+admin.site.register(Characteristic)
+admin.site.register(FootballClub)

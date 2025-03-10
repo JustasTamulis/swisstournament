8ff7709336ba0cb156ff88cda36a0750eadc3fdc
Justas T <justas.tam@gmail.com>
2025-03-10 13:18:50 +0200

    reorder tournament.py

--- backend/api/tournament.py
+++ backend/api/tournament.py
@@ -1,6 +1,8 @@
 import random
 from .models import Round, Team, Game, Odds, Bet, Bonus
 
+# Betting stage
+
 def generate_new_odds(round_id):
     """Generate new odds for the given round. Currently all odds = 1.0"""
     round_obj = Round.objects.get(id=round_id)
@@ -15,47 +17,6 @@ def generate_new_odds(round_id):
             odd2=1.0
         )
     return True
-
-def generate_new_game_pairs(round_id):
-    """Generate random game pairs for the given round"""
-    round_obj = Round.objects.get(id=round_id)
-    teams = list(Team.objects.all())
-    random.shuffle(teams)
-    
-    # Pair teams randomly
-    games = []
-    for i in range(0, len(teams), 2):
-        if i + 1 < len(teams):
-            game = Game.objects.create(
-                team1=teams[i],
-                team2=teams[i+1],
-                round=round_obj,
-                finished=False
-            )
-            games.append(game)
-    
-    return games
-
-def move_track(team_id, distance=1):
-    """Update team's distance based on win"""
-    team = Team.objects.get(id=team_id)
-    team.distance += distance
-    team.save()
-    return team.distance
-
-def give_bonus(team_id, bonus=0):
-    """Give bonus to a team - currently no bonus logic"""
-    team = Team.objects.get(id=team_id)
-    # In the future, implement bonus logic here
-    team.save()
-    return team
-
-def check_winner():
-    """Check if there's a winner (distance > 12)"""
-    winner = Team.objects.filter(distance__gt=12).first()
-    if winner:
-        return winner
-    return None
 
 def all_bets_placed(round_id):
     """Check if all teams have placed bets for this round"""
@@ -86,6 +47,28 @@ def move_to_next_stage(round_id):
     
     return new_round
 
+# Joust stage
+
+def generate_new_game_pairs(round_id):
+    """Generate random game pairs for the given round"""
+    round_obj = Round.objects.get(id=round_id)
+    teams = list(Team.objects.all())
+    random.shuffle(teams)
+    
+    # Pair teams randomly
+    games = []
+    for i in range(0, len(teams), 2):
+        if i + 1 < len(teams):
+            game = Game.objects.create(
+                team1=teams[i],
+                team2=teams[i+1],
+                round=round_obj,
+                finished=False
+            )
+            games.append(game)
+    
+    return games
+
 def all_games_finished(round_id):
     """Check if all games in the round are finished"""
     round_obj = Round.objects.get(id=round_id)
@@ -94,6 +77,13 @@ def all_games_finished(round_id):
         if not game.finished:
             return False
     return True
+
+def move_track(team_id, distance=1):
+    """Update team's distance based on win"""
+    team = Team.objects.get(id=team_id)
+    team.distance += distance
+    team.save()
+    return team.distance
 
 def process_winners(round_id):
     """Process all winners of the round's games, increasing their distance"""
@@ -135,22 +125,7 @@ def move_to_bonus_stage(round_id):
     
     return new_round
 
-def move_to_finished_stage(round_id, winner):
-    """Create a final round marking the tournament as finished"""
-    current_round = Round.objects.get(id=round_id)
-    
-    # Create new round with finished stage
-    new_round = Round.objects.create(
-        number=current_round.number,
-        active=True,
-        stage="finished"
-    )
-    
-    # Set current round as inactive
-    current_round.active = False
-    current_round.save()
-    
-    return new_round
+# Bonus stage
 
 def all_bonuses_used(round_id):
     """Check if all teams have used their bonuses for this round"""
@@ -178,5 +153,31 @@ def move_to_new_round(round_id):
     
     # Generate odds for the new round
     generate_new_odds(new_round.id)
+    
+    return new_round
+
+# Finished stage
+
+def check_tournament_winner():
+    """Check if there's a winner (distance > 12)"""
+    winner = Team.objects.filter(distance__gt=12).first()
+    if winner:
+        return winner
+    return None
+
+def move_to_finished_stage(round_id, winner):
+    """Create a final round marking the tournament as finished"""
+    current_round = Round.objects.get(id=round_id)
+    
+    # Create new round with finished stage
+    new_round = Round.objects.create(
+        number=current_round.number,
+        active=True,
+        stage="finished"
+    )
+    
+    # Set current round as inactive
+    current_round.active = False
+    current_round.save()
     
     return new_round

774482d115c5e1bb2f5c4b8fdff7355d6b1fb6dd
Justas T <justas.tam@gmail.com>
2025-03-12 16:49:45 +0200

    Remove old frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,6 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router'
 import './App.css'
 
-// Old components
-import Home from './components_old/Home'
-import Create from './components_old/Create'
-import Edit from './components_old/Edit'
-import Delete from './components_old/Delete'
-import Navbar from './components_old/navbar/Navbar'
-
-// New components
 import TopNav from './components/navbar/TopNav'
 import JoustPage from './components/pages/JoustPage'
 import BetPage from './components/pages/BetPage'
@@ -19,45 +10,23 @@ import AboutPage from './components/pages/AboutPage'
 
 // Tournament Context
 import { TournamentProvider } from './context/TournamentContext';
-
 function App() {
   return (
-    <>
-      <Routes>
-        {/* New tournament routes - added wildcard matcher (/*) to parent route */}
-        <Route path="/*" element={
-          <TournamentProvider>
-            <TopNav
-              content={
-                <Routes>
-                  {/* Default route redirects to track now */}
-                  <Route path="" element={<TrackPage />} />
-                  <Route path="track" element={<TrackPage />} />
-                  <Route path="bet" element={<BetPage />} />
-                  <Route path="joust" element={<JoustPage />} />
-                  <Route path="bonus" element={<BonusPage />} />
-                  <Route path="about" element={<AboutPage />} />
-                </Routes>
-              }
-            />
-          </TournamentProvider>
-        } />
-
-        {/* Old routes moved to /old/* */}
-        <Route path="/old/*" element={
-          <Navbar
-            content={
-              <Routes>
-                <Route path="" element={<Home />} />
-                <Route path="create" element={<Create />} />
-                <Route path="edit/:id" element={<Edit />} />
-                <Route path="delete/:id" element={<Delete />} />
-              </Routes>
-            }
-          />
-        } />
-      </Routes>
-    </>
+    <TournamentProvider>
+      <TopNav
+        content={
+          <Routes>
+            {/* Default route redirects to track */}
+            <Route path="/" element={<TrackPage />} />
+            <Route path="track" element={<TrackPage />} />
+            <Route path="bet" element={<BetPage />} />
+            <Route path="joust" element={<JoustPage />} />
+            <Route path="bonus" element={<BonusPage />} />
+            <Route path="about" element={<AboutPage />} />
+          </Routes>
+        }
+      />
+    </TournamentProvider>
   )
 }
 
